11635972d8fda17424dc7a36cff464d5b00cf48c
arka816 <arka816@iitkgp.ac.in>
2021-04-22 20:35:55 +0530

    cut functionality added

--- src/Editor/editor.js
+++ src/Editor/editor.js
@@ -5,6 +5,8 @@ import Container from './state/state.js';
 import CodeInput from "./components/codeinput.js";
 import TextSearch from "./components/textsearch";
 import "../css/editor.css";
+var language = localStorage.getItem("language") || 'c';
+const tokenizer = require("./engine/" + language + "/tokenizer.js");
 
 function objectToArray(p){
     var keys = Object.keys(p);
@@ -57,6 +59,14 @@ const downloadFile = (codeString, filename) => {
     document.body.removeChild(a);
 }
 
+const findWidthofChar = (c) => {
+    c = c.replaceAll(" ", "\u00a0");
+    var elem = document.getElementById('text_width_finder');
+    elem.innerText = c;
+    let width = elem.getBoundingClientRect().width;
+    return width;
+}
+
 
 const Editor = () => {
     const [findMode, setFindMode] = useState(false);
@@ -67,8 +77,49 @@ const Editor = () => {
         }
     }, [Store, Store.contentArray, Store.tokenArray])
 
+    const updateStorage = () => {
+    	localStorage.setItem('contentArray', JSON.stringify(Store.contentArray));
+    	localStorage.setItem('tokenArray', JSON.stringify(Store.tokenArray));
+    }
+
+    const clearSelection = (x1, y1, x2, y2) => {
+        for(let line = x1 + 1; line < x2; line++){
+            Store.contentArray.splice(line, 1);
+            Store.tokenArray.splice(line, 1);
+            Store.setLineCount(Store.lineCount - 1);
+        }
+
+        let c1 = Store.contentArray[x1].slice(0, y1);
+        let c2 = Store.contentArray[x2].slice(y2);
+        Store.contentArray[x1] = c1 + c2;
+        Store.tokenArray[x1] = tokenizer.tokenize(c1 + c2);
+
+        Store.contentArray.splice(x2, 1);
+        Store.tokenArray.splice(x2, 1);
+        Store.setLineCount(Store.lineCount - 1);
+
+        updateStorage();
+    }
+
+    const cutToClipboard = (e) => {
+        const [anchor_line, anchor_content_index, focus_line, focus_content_index] = copyToClipboard(e);
+        clearSelection(anchor_line, anchor_content_index, focus_line, focus_content_index);
+        if (window.getSelection().empty) {  
+            // Chrome
+            window.getSelection().empty();
+        } else if (window.getSelection().removeAllRanges) {  
+            // Firefox
+            window.getSelection().removeAllRanges();
+        }
+        Store.setCursorLine(anchor_line);
+        Store.setCursorIndex(anchor_content_index);
+        Store.setCursorOffset(findWidthofChar(Store.contentArray[anchor_line].slice(0, anchor_content_index)));
+        document.getElementById('edit_textarea').focus();
+    }
+
     const copyToClipboard = (e) => {
         e.preventDefault();
+        e.stopPropagation();
         let selection = window.getSelection();
         if(selection.toString().length > 0){
             let anchor_line = parseInt(selection.anchorNode.parentElement.parentElement.dataset.line);
@@ -92,18 +143,24 @@ const Editor = () => {
             for(var i = anchor_index + 1; i < Store.tokenArray[anchor_line].length; i++){
                 copiedText += Store.tokenArray[anchor_line][i][0];
             }
+            let anchor_content_index = Store.contentArray[anchor_line].length - copiedText.length;
             copiedText += "\n";
             for(let i = anchor_line + 1; i < focus_line; i++){
                 copiedText += Store.contentArray[i];
                 copiedText += "\n";
             }
+            let focus_content_index = 0;
             for(let i = 0; i < focus_index; i++){
+                focus_content_index += Store.tokenArray[focus_line][i][0].length;
                 copiedText += Store.tokenArray[focus_line][i][0];
             }
             copiedText += Store.tokenArray[focus_line][focus_index][0].slice(0, focus_offset);
-            //console.log(copiedText);
+            focus_content_index += focus_offset;
+
             e.clipboardData.setData('text/plain', copiedText);
+            return [anchor_line, anchor_content_index, focus_line, focus_content_index];
         }
+        return null;
     }
 
     const verbose = () => {
@@ -175,7 +232,7 @@ const Editor = () => {
                     )
                 })}
             </div>
-            <div className="editor_code_wrapper" onCopy={copyToClipboard} onKeyDown={keyHandler}>
+            <div className="editor_code_wrapper" onCopy={copyToClipboard} onCut={cutToClipboard} onKeyDown={keyHandler}>
                 {findMode ? <TextSearch setFindMode={setFindMode} /> : ""}
                 <CodeInput />
                 {lines.map((val, index) => {
