c6951a4817a849801c03bf26506d4b85d3d43025
arka816 <arka816@iitkgp.ac.in>
2021-04-22 21:13:51 +0530

    added nav to text search

--- src/Editor/components/textsearch.js
+++ src/Editor/components/textsearch.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Container from '../state/state.js';
 import SearchIcon from '@material-ui/icons/Search';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
@@ -6,7 +6,11 @@ import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import CloseIcon from '@material-ui/icons/Close';
 
 const TextSearch = ({setFindMode}) => {
+    const [found, setFound] = useState(false);
+    const [searchIndex, setSearchIndex] = useState(0);
+    const [highlightElems, setHighlightElems] = useState([]);
     const Store = Container.useContainer();
+
 
     const findWidthofChar = (c) => {
         c = c.replaceAll(" ", "\u00a0");
@@ -26,6 +30,8 @@ const TextSearch = ({setFindMode}) => {
         canvas.style.display="none";
         let context = canvas.getContext('2d');
         context.clearRect(0, 0, canvas.width, canvas.height);
+
+        setHighlightElems([]);
 
         setFindMode(false);
     }
@@ -65,6 +71,11 @@ const TextSearch = ({setFindMode}) => {
         ctx.lineWidth = 10;
         ctx.strokeStyle = "#10a115";
 
+        if(locations.length > 0) setFound(true);
+        else setFound(false);
+
+        let elems = [];
+
         for(line in locations){
             var codeline = document.getElementById('editor_code_line_' + line);
             
@@ -80,8 +91,14 @@ const TextSearch = ({setFindMode}) => {
                 span.style.left = findWidthofChar(Store.contentArray[line].slice(0, index)) + "px";
                 span.style.width = w + "px";
                 codeline.appendChild(span);
+                elems.push(span);
             }
         }
+
+        setHighlightElems(elems);
+        setSearchIndex(0);
+
+        elems[0].style.backgroundColor = "rgba(81, 109, 120, 0.5)";
     }
 
     const keyHandler = (e) => {
@@ -91,15 +108,43 @@ const TextSearch = ({setFindMode}) => {
         }
     }
 
+    const prevHighlight = () => {
+        highlightElems[searchIndex].style.backgroundColor = "rgba(8, 135, 8, 0.3)";
+        let nextIndex = (highlightElems.length + searchIndex - 1)%(highlightElems.length);
+        highlightElems[nextIndex].style.backgroundColor = "rgba(81, 109, 120, 0.5)";
+        setSearchIndex(nextIndex);
+    }
+
+    const nextHighlight = () => {
+        highlightElems[searchIndex].style.backgroundColor = "rgba(8, 135, 8, 0.3)";
+        let nextIndex = (searchIndex + 1)%(highlightElems.length);
+        highlightElems[nextIndex].style.backgroundColor = "rgba(81, 109, 120, 0.5)";
+        setSearchIndex(nextIndex);
+    }
+
+    const searchNavHandler = (e) => {
+        e.preventDefault();
+        var key = e.keyCode || e.charCode;
+
+        if(key === 39 || key === 40){
+            nextHighlight();
+            e.stopPropagation();
+        }
+        else if(key === 37 || key === 38){
+            prevHighlight();
+            e.stopPropagation();
+        }
+    }
+
     return (
-        <div className="text_search">
+        <div className="text_search" onKeyDown={searchNavHandler}>
             <div className="text_input_container">
                 <input autoFocus placeholder="Find" id="text_search_input" onKeyDown={keyHandler} />
                 <SearchIcon style={{color: "white", position: "absolute", right: "4px", width: "20px"}} onClick={search} />
             </div>
             <div style={{position: "absolute", right: "4px", height: "24px", top: "4px"}}>
-                <KeyboardArrowUpIcon style={{color: "white", width: "20px"}} />
-                <KeyboardArrowDownIcon style={{color: "white", width: "20px"}} />
+                <KeyboardArrowUpIcon style={{color: found ? "white" : "#636360", width: "20px"}} onClick={prevHighlight} />
+                <KeyboardArrowDownIcon style={{color: found ? "white" : "#636360", width: "20px"}} onClick={nextHighlight} />
                 <CloseIcon style={{color: "white", width: "20px"}} onClick={close} />
             </div>
         </div>
